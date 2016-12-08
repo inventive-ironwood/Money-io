efffe74d77eac9a03d8cdf989bbfc81bb0589c57
Hack Reactor Students <accounts@hackreactor.com>
2016-12-07 21:27:43 -0800

    Extends toggling function between different features

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,11 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    //this.click = this.click.bind(this);
-    this.data = props.data;
+    this.click = this.click.bind(this);
+    this.state ={
+      curr: (<SpendingList list={props.data} />),
+      data: props.data
+    };
     this.render();
   }
   // click(vid) {
@@ -23,12 +26,23 @@ class App extends React.Component {
   // search(e) {
   //   this.props.searchYouTube({key: window.YOUTUBE_API_KEY, query: e.target.value, max: 8}, this.caller);
   // }
+  click(e) {
+    console.log(e.target.innerHTML);
+    if(e.target.innerHTML === 'Spending') {
+      this.setState({ 
+        curr: (<SpendingList list={this.state.data} />)
+      })
+    } else if(e.target.innerHTML === 'Owes/Debts') {
+      this.setState({
+        curr: (<OwesList list={this.state.data} />) 
+      });
+    }
+  }
   render() {
     return (
       <div className="app">
-        <Sidebar />
-        <SpendingList list={this.data} />
-        <OwesList list={this.data} />
+        <Sidebar click={this.click}/>
+        {this.state.curr}
       </div>
     );
   }
