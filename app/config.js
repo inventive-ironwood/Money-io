155bf09d4c1f9434cc578336eb3260fcd71c11a4
Hack Reactor Students <accounts@hackreactor.com>
2016-12-08 20:31:10 -0800

    Add database

---
+++ app/config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var knex = require('knex')({
+ client: 'sqlite3',
+ connection: {
+   filename: path.join(__dirname, '../db/moneyio.sqlite')
+ },
+ useNullAsDefault: true
+});
+
+var db = require('bookshelf')(knex);
+
+ 
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255);
+      user.string('password', 255);
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+ 
+db.knex.schema.hasTable('spending').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('spending', function (entry) {
+      entry.string('category', 255);
+      entry.string('title', 255);
+      entry.float('amount');
+      entry.timestamps('date');
+      entry.integer('user_id').references('users.id');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+
+db.knex.schema.hasTable('debts').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('debts', function (entry) {
+      entry.string('type', 255);
+      entry.integer('person').references('users.id')
+      entry.float('amount');
+      entry.timestamps('date');
+      entry.integer('user_id').references('users.id');
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+module.exports = db;
