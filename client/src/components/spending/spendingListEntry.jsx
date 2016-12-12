c991e5cc1c2f7414dbc0f9de1f22127d2d8eb7cb
Dunstan Zheng <dunstanzheng@gmail.com>
2016-12-12 10:34:54 -0800

    Have fixed functionality and category icons

--- client/src/components/spending/spendingListEntry.jsx
+++ client/src/components/spending/spendingListEntry.jsx
@@ -1,10 +1,9 @@
 var SpendingListEntry = (props) => (
   <tr>
-    <td><img src={props.entry.icon} ></img></td>
-    <td>12/8/16 - 7:13PM</td>
+    <td><img src={props.entry.icon}></img></td>
     <td>{props.entry.title}</td>
     <td>{props.entry.price}</td>
-    <td>Placeholder</td>
+    <td>1 hour ago</td>
   </tr>
   );
 
--- src/components/owes/owesList.jsx
+++ src/components/owes/owesList.jsx
@@ -4,8 +4,8 @@ var OwesList = (props) => (
     <table className="table">
       <thead id="owes-head">
         <tr>
+          <th>Category</th>
           <th>Title</th>
-          <th>Who</th>
           <th>Amount</th>
           <th>Date</th>
           <th>Option</th>
